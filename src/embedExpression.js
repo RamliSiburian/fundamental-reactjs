97bfebc4fcc6fabe38003d98e3fb6bda6df93a05
RamliSiburian <ramlisiburian1111@gmail.com>
2022-10-13 09:59:26 +0700

    Add Embed Expression

--- src/embedExpression.js
+++ src/embedExpression.js
@@ -2,12 +2,19 @@ import React from "react";
 
 function EmbedExpression() {
   //Create function here
+  function Embed() {
+    return 'This is embed from function'
+  }
 
   //Create a variable here
+  const varEmbed = 'This is embed from Variable'
 
   return (
     // Code Inside div
-    <div></div>
+    <div>
+      <p>{Embed()}</p>
+      <p>{varEmbed}</p>
+    </div>
   );
 }
 
