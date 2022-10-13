ccdefd177d153cd9f537f50af8340c0501b65773
RamliSiburian <ramlisiburian1111@gmail.com>
2022-10-13 10:17:14 +0700

    Add Event

--- src/event.js
+++ src/event.js
@@ -2,10 +2,22 @@ import React from "react";
 
 function Event() {
   //Create function here
+  function ExEventFunc() {
+    return alert('Event Start When Button Clicked With Function')
+
+  }
 
   return (
     // Code Inside div
-    <div></div>
+    <>
+      <div>
+        <button onClick={() => alert('Event Start When Button Clicked With Arrow Function')}>Click Here</button>
+      </div>
+      <br />
+      <div>
+        <button onClick={ExEventFunc}>Press Here</button>
+      </div>
+    </>
   );
 }
 
